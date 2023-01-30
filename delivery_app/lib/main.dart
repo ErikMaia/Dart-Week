5255e8196835ac76b12f7008a9bf62ddccadd4e5
Erik Silva Maia <erikm384@gmail.com>
2023-01-30 16:05:11 -0300

    feat: implement dotenv

--- delivery_app/lib/main.dart
+++ delivery_app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'App/Core/config/env.dart';
 import 'App/dw9_delivery_app.dart';
 
-void main() {
+Future<void> main() async {
+  await Env.instance.load();
   runApp(const Dw9DeliveryApp());
 }
