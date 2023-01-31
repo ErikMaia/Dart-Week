47448ff8ec6fdd1e26e6ee88cf071698df197a4d
Erik Silva Maia <erikm384@gmail.com>
2023-01-31 03:32:17 -0300

    feat: create fist page

--- delivery_app/lib/App/pages/splash/splash_page.dart
+++ delivery_app/lib/App/pages/splash/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:dw9_delivery_app/App/Core/ui/helper/size_extentions.dart';
 import 'package:dw9_delivery_app/App/Core/ui/widget/delivery_button.dart';
 import 'package:flutter/material.dart';
 
@@ -8,7 +9,28 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Splash Page")),
-      body: Column(children: [DeliveryButton(label: "Button", onPressed: () {  },),TextFormField()]),
+      body: ColoredBox(
+        color: const Color(0xff140e0e),
+        child: Stack(children: [
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SizedBox(
+              width: context.screenWidth,
+              child: Image.asset("assets/images/lanche.png", fit: BoxFit.cover),
+            ),
+          ),
+          Center(
+            child: Column(
+              children: [
+                SizedBox(height: context.percentHeight(0.3)),
+                Image.asset("assets/images/logo.png"),
+                const SizedBox(height: 30),
+                DeliveryButton(label: "ACESSAR", onPressed: (){}, height: 35, width: context.percentWidth(.6),)
+              ],
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
